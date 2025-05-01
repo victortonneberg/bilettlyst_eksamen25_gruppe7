fbd54b5eb2914782601f9f7552826032ee0a9dc8
Victor Tønneberg <victon2003@gmail.com>
2025-05-01 19:40:19 +0200

    Dashboard C-krav
    UseState for å håndtere logg-inn.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react"
 import "../assets/styles/Dashboard/Dashboard.scss"
 
 export default function Dashboard() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setIsLoggedIn(true)
+  }
+
   return (
     <>
-      <section id="loginform">
-        <h1>Dashboard</h1>
-        <form>
-          <h2>Logg inn</h2>
-          <label>
-            Brukernavn
-            <input type="text" name="username" placeholder="victorto" />
-          </label>
-          <input
-            type="submit"
-            value="Logg inn"
-            className="submitbutton"
-          ></input>
-        </form>
-      </section>
+      {!isLoggedIn ? (
+        <section id="loginform">
+          <h1>Dashboard</h1>
+          <form onSubmit={handleSubmit}>
+            <h2>Logg inn</h2>
+            <label>
+              Brukernavn
+              <input type="text" name="username" placeholder="victorto" />
+            </label>
+            <input
+              type="submit"
+              value="Logg inn"
+              className="submitbutton"
+            ></input>
+          </form>
+        </section>
+      ) : (
+        <h1>Min side</h1>
+      )}
     </>
   )
 }
