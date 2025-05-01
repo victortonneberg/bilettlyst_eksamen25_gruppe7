64aa002642f71a9976af677554e39f16c8b13752
kristian <Kristian.larsen38@gmail.com>
2025-05-01 16:11:38 +0200

    Lagt inn filter og søk på CategoryPage

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -1,5 +1,30 @@
     export default function CategoryPage() {
+
+
         return(
-            <h1>hallo</h1>
+            <>
+                <h2>Filtrert søk</h2>
+                <section id="categoryPage-filter">
+                    <p>Dato:</p>
+                    <input type="date"></input>
+                    <p>Land:</p>
+                    <select name="Land" id="country">
+                        <option value="Norge">Norge</option>
+                        <option value="Sverige">Sverige</option>
+                        <option value="Danmark">Danmark</option>
+                    </select>
+                    <p>By:</p>
+                    <select name="By" id="city">
+                        <option value="Oslo">Oslo</option>
+                        <option value="Stockholm">Stockholm</option>
+                        <option value="København">København</option>
+                    </select>
+                </section>
+                <h2>Søk</h2>
+                <section>
+                    <p>Søk etter event, attraksjon eller spillested</p>
+                    <input type="text" />
+                </section>
+            </>
         )
     }
