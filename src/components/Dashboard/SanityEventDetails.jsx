2c8affd87d4e8b2dc5a6b2144718e4009c3f8222
Victor Tønneberg <victon2003@gmail.com>
2025-05-03 20:12:46 +0200

    Wishlist funksjonalitet
    Lister nå opp hvilke brukere som har eventet i ønskeliste.

--- src/components/Dashboard/SanityEventDetails.jsx
+++ src/components/Dashboard/SanityEventDetails.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { fetchSingleEvent } from "../../sanity/fetchSingleEvent"
+import { fetchUsers } from "../../sanity/fetchUsers"
 
 export default function SanityEventDetails() {
   const { id } = useParams()
   const [event, setEvent] = useState(null)
   const [ticketmasterData, setTicketmasterData] = useState(null)
+  const [users, setUsers] = useState([])
 
   const getSanityEvent = () => {
     fetchSingleEvent(id)
@@ -27,14 +29,31 @@ export default function SanityEventDetails() {
       .catch((error) => console.log("Feil under fetch fra TM: ", error))
   }
 
+  const getUsers = () => {
+    fetchUsers()
+      .then(setUsers)
+      .catch((error) =>
+        console.log("Feil ved henting av users fra Sanity: ", error)
+      )
+  }
+
   useEffect(() => {
     getSanityEvent()
     getTicketmasterData()
+    getUsers()
   }, [id])
 
   if (!event) {
     return <p>Arrangementet finnes ikke....</p>
   }
+
+  // Kilde for .some: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+  const addedInWishlist = users.filter((user) =>
+    user.wishlist?.some((wishlistItem) => wishlistItem.apiId === id)
+  )
+
+  console.log("Ønskeliste:", addedInWishlist)
+  console.log("Users: ", users)
 
   return (
     <>
@@ -49,6 +68,21 @@ export default function SanityEventDetails() {
         {ticketmasterData?.classifications?.map((item, index) => (
           <p key={index}>{item.segment?.name}</p>
         ))}
+      </section>
+      <section className="inWishlist">
+        <h2>Andre med dette arrangementet i ønskelisten</h2>
+        <ul>
+          {addedInWishlist.map((user) => (
+            <li key={user._id}>
+              <img
+                src={user.imageUrl}
+                alt={user.name}
+                width="100" /*KORRIGER I SCSS!*/
+              ></img>
+              <p>{user.name}</p>
+            </li>
+          ))}
+        </ul>
       </section>
     </>
   )
