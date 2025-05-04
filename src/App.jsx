6c68d1cbe64400f7fb14a2bcad422d04b871eb67
Victor Tønneberg <victon2003@gmail.com>
2025-05-04 20:34:37 +0200

    Småfix etter merge
    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import CategoryPage from "./components/CategoryPage/CategoryPage"
 import EventPage from "./components/EventPage/eventPage"
