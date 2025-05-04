4890e4b6904dfa74132e3dc282eaa55c3e0cccba
Victor Tønneberg <victon2003@gmail.com>
2025-05-04 20:31:10 +0200

    Global header
    +SCSS mobile + desktop

---
+++ src/components/Global_C/Header.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+import "../../assets/styles/Global_C/Header.scss"
+
+export default function Header() {
+  return (
+    <header className="globalheader">
+      <Link to="/" className="logo">
+        BilettLyst
+      </Link>
+      <nav className="global-nav">
+        <ul className="nav-center">
+          <li>
+            <Link to="/category/musikk">Musikk</Link>
+          </li>
+          <li>
+            <Link to="/category/sport">Sport</Link>
+          </li>
+          <li>
+            <Link to="/category/teater">Teater/Show</Link>
+          </li>
+        </ul>
+        <ul className="header-min-side">
+          <li>
+            <Link to="/dashboard">Min side</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
