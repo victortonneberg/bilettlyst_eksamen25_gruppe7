2a0c17228f67f07addf667e202071eeab43504a3
AFKJonas <jonasengelsgaard@gmail.com>
2025-05-06 16:05:52 +0200

    Rettet skrivefeil

--- src/components/HomePage/FestivalCard.jsx
+++ src/components/HomePage/FestivalCard.jsx
@@ -7,7 +7,7 @@ export default function FestivalCard({ event }) {
   const imageUrl = event.images?.[0]?.url
 
   return (
-    <Link to={`/event/${event.id}`} className="festivalCard-Link">
+    <Link to={`/events/${event.id}`} className="festivalCard-Link">
       <article className="festivalCard">
         {imageUrl && (
           <img
