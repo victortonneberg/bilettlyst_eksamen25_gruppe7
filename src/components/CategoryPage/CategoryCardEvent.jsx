8515b62be9fb98c71173a3797ca748415ca44d5c
kristian <Kristian.larsen38@gmail.com>
2025-05-08 10:50:54 +0200

    fiksa event kort

--- src/components/CategoryPage/CategoryCardEvent.jsx
+++ src/components/CategoryPage/CategoryCardEvent.jsx
@@ -3,9 +3,7 @@ import { faStar as faSolidStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faRegularStar } from '@fortawesome/free-regular-svg-icons';
 
 export default function CategoryCardEvent({ event, isFavourite, toggleFavourite }) {
-    const formattedDate = event.date ? new Date(event.date).toLocaleDateString() : "Ukjent dato";
-    const formattedTime = event.time ? new Date(`1970-01-01T${event.time}`).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : "Ukjent tid";
-    
+  
     return (
         <article className="eventCard">
             <img src={event.image} alt={"Bilde ikke funnet:  " + event.name} />
@@ -18,8 +16,7 @@ export default function CategoryCardEvent({ event, isFavourite, toggleFavourite 
             </button>
             <h3>{event.name}</h3>
             <p>{event.date}</p>
-            <p>Dato: {formattedDate}</p>
-            <p>Tid: {formattedTime}</p>
+            <p>{event.time}</p>
         </article>
     );
 }
